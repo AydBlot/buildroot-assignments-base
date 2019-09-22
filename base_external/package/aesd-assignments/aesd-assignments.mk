689ff3821c6d6936fa5a363ee3774d3efd5f7626
Linfeng Li <linfeng.li@colorado.edu>
2019-09-22 07:39:13 -0600

    add necessary files in order to include br2-external package tree and make the default defconfig

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,8 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_VERSION = 2620c7457b07735d1b1585dbd7d6100991cb4c35
+AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-5013/aesd-assignments-3-and-later-shykn1
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
@@ -18,6 +18,8 @@ endef
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
 endef
 
 

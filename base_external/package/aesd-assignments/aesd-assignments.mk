6f8fbbad7663f7582255da07fff3f352bcca18a3
Linfeng Li <linfeng.li@colorado.edu>
2019-11-01 23:12:20 -0600

    forward to the latest

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,14 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = e6d6e7cd9d43439610056c0cd5cce731016177cf
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-5013/aesd-assignments-3-and-later-shykn1
+AESD_ASSIGNMENTS_VERSION = 2edcf2e91cbe0eb8f5ea3a53f50aa622e2cf7ef2
+AESD_ASSIGNMENTS_SITE =  http://github.com/cu-ecen-5013/aesd-assignments-3-and-later-shykn1
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CROSS) -C  $(@D) all
 endef
@@ -20,7 +23,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/start_stop_daemon.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 LDD_VERSION = 6d16ea94bb7dd2636264c2c739c53acb8dfd754d
-LDD_SITE = https://github.com/cu-ecen-5013/assignment-7-buildroot-and-yocto-kernel-builds-shykn1
+LDD_SITE = git://github.com/cu-ecen-5013/assignment-7-buildroot-and-yocto-kernel-builds-shykn1
 LDD_SITE_METHOD = git
 
 #LDD_SITE = /home/linfeng/Downloads/AESD/assignment-7-buildroot-and-yocto-kernel-builds-shykn1/scull

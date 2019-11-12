2396fe55ff608bfe37acc7ecf809ac3b0ff9ba12
Linfeng Li <linfeng.li@colorado.edu>
2019-11-11 20:05:58 -0700

    update aesd-assignments package version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c720306388071083ab8b39cdcabce7689102c502
+AESD_ASSIGNMENTS_VERSION = 32dd0111f395ac0ac582247998f03af14d1a67ed
 AESD_ASSIGNMENTS_SITE =https://github.com/cu-ecen-5013/aesd-assignments-3-and-later-shykn1.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -23,7 +23,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/drivertest-assignment-9.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load_qemu $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/start_stop_daemon.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
